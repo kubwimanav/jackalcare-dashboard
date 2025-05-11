f9fd64fdb5744c37ca055c7ac7fb49b18f64cc7c
kubwimanav <kubwimanavincent2020@gmail.com>
2025-05-11 19:02:15 +0200

    fixerror

--- src/landingpage/HeroSection/HeroSection.jsx
+++ src/landingpage/HeroSection/HeroSection.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import doc from "../../../src/assets/doc.jpg";
 const HeroSection = () => {
   return (
     <div className="relative w-full h-screen md:h-[600px] lg:h-[700px]">
       <img
-        src={doc}
+        src="/Doc.jpg"
         alt="Hero"
         className="w-full h-full object-cover"
       />
-      {/* Optional: Add overlay or text content */}
     </div>
   );
 };
